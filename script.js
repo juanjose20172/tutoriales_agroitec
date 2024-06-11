77c2e747edaf2fd97673db220b82bc22c738869d
juanjose2019 <32147599+juanjose20172@users.noreply.github.com>
2024-06-10 23:45:42 -0500

    adiciona pagina evitar whatsapp

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-function updateDate() {
-    const dateInput = document.getElementById('dateInput').value;
-    const dateElement = document.getElementById('date');
-    
-    if (dateInput) {
-        const date = new Date(dateInput);
-        const options = { day: '2-digit', month: 'short', year: 'numeric' };
-        const formattedDate = date.toLocaleDateString('es-ES', options).replace('.', '').replace(' ', '/').replace(' ', '/');
-        dateElement.textContent = formattedDate;
-	 
-    }
-}
-
-function updatePrice() {
-    const priceInput = document.getElementById('priceInput').value;
-    const priceElement = document.getElementById('price');
-    
-    if (priceInput) {
-        priceElement.textContent = `$${parseInt(priceInput).toLocaleString('es-CO')} COP`;
-    }
-}
-
 function hideInputs() {
-    document.getElementById('inputContainer').style.display = 'none';
-    document.getElementById('hideButton').style.display = 'none';
+    // document.getElementById('hideButton').style.display = 'none';
+    document.getElementById('videoIframe').style.display = 'block';
+    document.getElementById('videoIframe').scrollIntoView({ behavior: 'smooth' });
 }
